cfcc9b0d9826233c0e3e56de9f1a39ca83b1ce21
fernando.furibeiro <fernando.furibeiro@ITA0557232W10-1>
2024-08-06 16:40:28 -0300

    Adicionar arquivos de projeto.

---
+++ aprendendocsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");               // Escreval
+            Console.Write("Pressione Qualquer Tecla...");   // Escreva mesma linha
+            Console.ReadKey();                              // Ler com qualquer tecla digitada
+            //Console.Read();                               // Ler após clicar Enter
+        }
+    }
+}
